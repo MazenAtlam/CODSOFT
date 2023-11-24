378f0999451b5d2700cc0f60548337c43898905d
MazenAtlam <mazenatef5510@gmail.com>
2023-11-24 18:38:46 +0200

    number guessing game[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dtask1:[Cn[3~[3~[3[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C

---
+++ task1.cpp
@@ -0,0 +1,62 @@
+#define CLOSE 50
+#define HOT 10
+#define MIN -100	// the minimum number
+#define MAX 100 	// the maximum number
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * main - Entry point
+ *
+ * Description: A number guessing game
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, rnum = MIN + (rand() % (MAX - MIN + 1));
+	bool first_guess, play_again;
+
+	do
+	{
+		first_guess = true;
+		cout << "Welcome to the number guessing game" << endl;
+		cout << "I have a number, try to guess it" << endl;
+		cout << "Number range is [" << MIN << " : " << MAX << "]" << endl;
+		do
+		{
+			if (!first_guess)
+				cout << "\nNever give up\nTry Again!" << endl;
+			cout << "Enter a guess: ";
+			cin >> n;
+			first_guess = false;
+			if (n < MIN || n > MAX)
+				cout << "The number is out of range" << endl;
+			else if (n > rnum)
+			{
+				if ((n - CLOSE) > rnum)
+					cout << "You are too high" << endl;
+				else if ((n - HOT) > rnum)
+					cout << "You are still high, but closer!" << endl;
+				else
+					cout << "Nice one!\nYou are still high, but that's very close!!" << endl;
+			}
+			else if (n < rnum)
+			{
+				if ((n + CLOSE) < rnum)
+                                        cout << "You are too high" << endl;
+                                else if ((n + HOT) < rnum)
+                                        cout << "You are still low, but closer!" << endl;
+                                else
+                                        cout << "Nice one!\nYou are still low, but that's very close!!" << endl;
+			}
+		} while (n != rnum);
+		cout << "Congratulations, that's the right guess!" << endl;
+		cout << "Enter 0 if you want to play again or a non-zero value to exit: ";
+		cin >> play_again;
+	} while (!play_again);
+
+	return 0;
+}
